4c1b4c382185700b38c3d5e7feaba62dd726330a
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-19 02:06:02 +0100

    Hello world

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,5 +1,5 @@
 #print("Programming is like building a multilingual puzzle")
-print ('Programming is like building a multilingual puzzle')
+print ('"Programming is like building a multilingual puzzle')
 
 
 
