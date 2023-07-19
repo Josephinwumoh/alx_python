c2b4fbf5720bb5654badd3c9a66d28ae7b7cf92c
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-19 06:24:50 +0100

    Updated.

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
 
-print("The string last digit of", number, "is", last_digit, end=" ")
+print("Last digit of", number, "is", last_digit, end=" ")
 
 
 if last_digit > 5:
@@ -15,4 +15,6 @@ elif last_digit == 0:
 
 else:
    print("and is less than 6 and not 0")
+
+   
 
