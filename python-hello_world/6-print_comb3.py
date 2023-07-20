6034d3dea50f0c7fd4fe086758fff89c9d228d8d
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-20 16:42:45 +0100

    Try again.

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-for p in range(0, 9):
-    for w in range(1, 10):
+
+for p in range(0, 10):
+    for w in range(p+1, 10):
         if p >= w:
             continue
         elif p == 8 and w == 9:
             print("{}{}".format(p, w))
         else:
              print("{}{}".format(p, w), end= ",")
+
