4209ef6ccb2102e81187237989b33f909a264e84
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-20 15:44:46 +0100

    New Changes made.

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d},".format(tens_digit*10+ones_digit), end="")
+for p in range(0, 9):
+    for w in range(1, 10):
+        if p >= w:
+            continue
+        elif p == 8 and w == 9:
+            print("{}{}".format(p, w))
+        else:
+             print("{}{}".format(p, w), end= ",")
 
 
         
