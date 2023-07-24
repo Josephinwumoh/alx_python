5c4a15828abae13577edf7abfc5fcb021c7818a2
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-24 20:57:18 +0100

    New changes

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,27 +1,3 @@
 #! /usr/bin/python3
 def add(a, b):
-
-  """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    
-    
-    
-    """
-  return a + b 
-
-def FAKE(a, b):
-   
-
-  return  a - b
-  
-print(__name__)
-print(FAKE(1, 2))
-print(FAKE(10, 30))
-print(FAKE(-10, 30))
-print(FAKE(-10, -30))
+  return a + b
