8bc47f8ffc9e802a1a5a330e56868cff0a0dd8d4
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-24 15:18:27 +0100

    New changes

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -13,12 +13,14 @@ def add(a, b):
     
     
     """
-
   return a + b 
-  
-def FAKE(a, b):
-  
-  """FAKE!"""
-  return a-b
 
+def sub(a, b):
+
+  return a - b
   
+print(__name__)
+print(sub(1, 2))
+print(sub(10, 30))
+print(sub(-10, 30))
+print(sub(-10, -30))
