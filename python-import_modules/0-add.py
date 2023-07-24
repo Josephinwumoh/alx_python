4afe5c036614a0c5e38e52d828e52b893d63b0d6
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-24 05:18:56 +0100

    New changes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,17 @@
 #! /usr/bin/python3
 
-from add_0 import add
-a = 1
-b = 2
-add(a,b)
-print("call function")
-print("{} + {} = {}".format(a, b, add(1,2)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    add(a, b)
+    print("call function")
+    print("{} + {} = {}".format(a, b, add(1,2)))
+
+
+
+
+
 
 
 
