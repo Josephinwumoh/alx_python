d9dad585c728f3aa9b6a5c0d085f7b2b34ff9507
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-24 05:05:42 +0100

    changes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,14 @@
 #! /usr/bin/python3
 
 from add_0 import add
-
 a = 1
-
 b = 2
+add(a,b)
+print("call function")
+print("{} + {} = {}".format(a, b, add(1,2)))
 
-result = add(a, b)
 
-print("{} + {} = {}".format(a, b, result))
+
 
 
 
