1568d96b03de3646e3e1e1b50a514fe8fa0ed9c3
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-29 04:04:24 +0100

    matrix functions.

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,10 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, element in enumerate(row):
+            if i < len(row) - 1:
+
+                print("{:d}".format(element), end=" ")
+            
+            else:
+
+                print("{:d}".format(element))
