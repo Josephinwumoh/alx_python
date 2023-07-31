55a4bfe22530776df982a22eb6aea3f3c0c45a29
Josephinwumoh <Jinwumoh@gmail.com>
2023-07-31 17:46:26 +0100

    Common elements.

--- 1-common_elements.py
+++ 1-common_elements.py
@@ -1,6 +1,4 @@
 def common_elements(set_1, set_2):
-    common_set = set()
-    for element in set_1:
-        if element in set_2:
-            common_set.add(element)
+    common_set = set_1 & set_2
+    
     return common_set
