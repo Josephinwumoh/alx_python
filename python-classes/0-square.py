afce3a08dbe30ecf186dd4e8b39c459da6cda9c8
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-01 16:03:26 +0100

    class and object.

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+class Square:
+    """This class represent square of sizes"""
+    def __init__(self, size):
+        """size is a private instance attributes"""
+        self.__size = size
+        
