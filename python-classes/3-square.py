69e4f3cb716a906e1332f2938d2af6e0f336e946
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-01 19:50:09 +0100

    To access private attributes.

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,24 +1,19 @@
-"""To Access an Update Private Attribute"""
+"""To access and update private attribute"""
 class Square:
-    """This represents the private instance
-      attribute for size"""
     def __init__(self, size=0):
-        """This shows the initialization for
-        the square is zero"""
-        self.size = size
+        self.__size = size
+
+    @property
     def size(self):
-        """Here is Getter Method to 
-        validate the assignment of a private attribute"""
         return self.__size
+
+    @size.setter
     def size(self, value):
-        """Here is the Setter Method used in 
-        defining how getting the attribute value"""
         if not isinstance(value, int):
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
-        """Returns the current square area for
-        Public instance method"""
-        return self.size ** 2
+        return self.__size ** 2
