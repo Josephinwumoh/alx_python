1dc4894eee8cd576d3f61bc611dbd54b4574de26
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 23:20:53 +0100

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -23,6 +23,12 @@ class BaseGeometry:
     def area(self):
         """Raise an exception that area is not implemented"""
         raise Exception("area() is not implemented")
+    
+    def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
 
 
         
