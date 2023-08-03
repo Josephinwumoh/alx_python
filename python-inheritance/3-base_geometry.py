7faf32a670e33880cfe3d87b55964fce17dbc41c
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 22:48:48 +0100

    A meta class to override the BaseGeometry.

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,18 +1,17 @@
 """BaseGeometry module with an empty class"""
 
-class BaseGeometry():
-        """ an empty class BaseGeometry"""
+class AMetaClass(type):
+        """ A Meta class with all the attributes"""
 
-        def __dir__(cls) -> None:
-                """getting lists of all attributes for this class and exclude __init_subclass"""
-                attributes = super().__dir__()
-                b_attributes = []
-                for attr in attributes:
-                        if attr != '__init_subclass__':
-                                b_attributes.append(attr)
-                return b_attributes
-        
         def __dir__(cls) -> None:
                 """Removing __init_subclass"""
                 attributes = super().__dir__()
-                return [attribute for attribute in attributes if attribute != '__init_subclass__']
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+class BaseGeometry(metaclass=AMetaClass):
+        """ an empty class BaseGeometry"""
+
+        def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
