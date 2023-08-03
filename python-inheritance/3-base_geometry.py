fe5e54493e9f05cc1e7103783fe33df886d91324
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 09:36:02 +0100

    new class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,7 +6,13 @@ class BaseGeometry():
         def __dir__(cls) -> None:
                 """getting lists of all attributes for this class and exclude __init_subclass"""
                 attributes = super().__dir__()
+                b_attributes = []
+                for attr in attributes:
+                        if attr != '__init_subclass__':
+                                b_attributes.append(attr)
+                return b_attributes
+        
+        def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
                 return [attribute for attribute in attributes if attribute != '__init_subclass__']
-
-
-
