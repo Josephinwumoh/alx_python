03f5d7e77e51b731bb402b24a334b3598051adc7
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 06:02:27 +0100

    An empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,5 @@
-"""Geometry module with an empty class"""
+"""BaseGeometry module with an empty class"""
 
 class BaseGeometry:
 
     """The BaseGeometry has an empty class"""
-
-bg = BaseGeometry()
-
-print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
