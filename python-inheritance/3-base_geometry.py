203ce594a9d3b817ef9bd5844571d57040ee5150
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 07:37:37 +0100

    An empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,5 @@
 """BaseGeometry module with an empty class"""
-class BaseGeometry:
+class BaseGeometry():
         """ an empty class BaseGeometry"""
 
         def __dir__(cls) -> None:
@@ -7,12 +7,11 @@ class BaseGeometry:
                 """get rid of all attributes for this class and exclude __init__subclass"""
 
                 attributes = super().__dir__()
-
                 J_attributes = []
                 for attr in attributes:
                         if attr != "__init_Subclass__":
                                 J_attributes.append(attr)
-                                
+                                pass   
                 return J_attributes
                                 
 
