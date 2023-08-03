3016a3cdbe435020e19371906fbdefa3f81493b1
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 23:03:33 +0100

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,5 +1,21 @@
 """This class shows improve Geometry"""
 
+class AMetaClass(type):
+        """ A Meta class with all the attributes"""
+
+        def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+class BaseGeometry(metaclass=AMetaClass):
+        """ an empty class BaseGeometry"""
+
+        def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+        
 class BaseGeometry:
 
     """This shows public instance Method"""
