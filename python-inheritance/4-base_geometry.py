242d5901a5cb47007131f8aba79e801b1586078e
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-03 08:20:50 +0100

    The Area is not implemented.

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,13 @@
+"""This class shows improve Geometry"""
+
+class BaseGeometry:
+
+    """This shows public instance Method"""
+
+    def area(self):
+        """Raise an exception that area is not implemented"""
+        raise Exception("area() is not implemented")
+
+
+        
+ 
