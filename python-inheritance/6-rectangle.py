235d55fcbfe66502df2be81a251365dbec13099f
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-04 01:49:40 +0100

    A Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -17,6 +17,24 @@ class BaseGeometry(metaclass=AMetaClass):
                 attributes = super().__dir__()
                 return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+class BaseGeometry(metaclass=AMetaClass):
+    """Public instance method that raise an Exception"""
+
+    def area(self):
+        """Raise an exception that area is not implemented"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+            """This is an integer validator that assign value"""
+            if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name)) 
+    
+    def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
 class Rectangle(BaseGeometry):
         """A rectangle with width and height"""
@@ -30,11 +48,14 @@ class Rectangle(BaseGeometry):
                 
         def __str__(self):
                 "Return the string of the Rectangle"
-                return ("[Rectangle] {}/{}".format(self.width, self.height))
+                return ("self.width, self.height")
         
-        def area(self):
-                """Calculate the area of the rectangle"""
-                return self.width * self.height
+        def integer_validator(self, name, value):
+            """This is an integer validator that assign value"""
+            if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name))
         
         def __dir__(cls) -> None:
                 """Removing __init_subclass"""
