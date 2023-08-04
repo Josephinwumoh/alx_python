caa4e48eb6894c17d776b9543925d54293c3e935
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-04 08:29:49 +0100

    Inherit from Base

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -16,8 +16,33 @@ class BaseGeometry(metaclass=AMetaClass):
                 """Removing __init_subclass"""
                 attributes = super().__dir__()
                 return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+        
+class Rectangle(BaseGeometry):
+        """A rectangle with width and height"""
 
-class BaseGeometry:
+        def __init__(self, width, height):
+                """The width and height integers of the rectangle"""
+                self.__width = 0
+                self.__height = 0
+                self.integer_validator("width", width)
+                self.integer_validator("height", height)
+                self.__width = width
+                self.__height = height
+               
+        
+        def integer_validator(self, name, value):
+            """This is an integer validator that assign value"""
+            if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name)) 
+            
+        def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+            
+class BaseGeometry(metaclass=AMetaClass):
     """Public instance method that raise an Exception"""
 
     def area(self):
@@ -41,9 +66,13 @@ class Rectangle(BaseGeometry):
 
         def __init__(self, width, height):
                 """The width and height integers of the rectangle"""
-                self.integer_validator = ("width", width)
-                self.integer_validator = ("height", height)
-    
+                self.__width = 0
+                self.__height = 0
+                self.integer_validator("width", width)
+                self.integer_validator("height", height)
+                self.__width = width
+                self.__height = height
+               
         
         def integer_validator(self, name, value):
             """This is an integer validator that assign value"""
