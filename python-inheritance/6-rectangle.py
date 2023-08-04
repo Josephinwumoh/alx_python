1746a25cbf40fd26d77e44736b561fe251428ce6
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-04 09:58:38 +0100

    A subclass

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -17,32 +17,6 @@ class BaseGeometry(metaclass=AMetaClass):
                 attributes = super().__dir__()
                 return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
         
-class BaseGeometry(BaseGeometry):
-      """A class BaseGeometry with width and height"""
-
-      def __init__(self, width, height):
-                """The width and height integers of the rectangle"""
-                self.__width = 0
-                self.__height = 0
-                self.integer_validator("width", width)
-                self.integer_validator("height", height)
-                self.__width = width
-                self.__height = height
-               
-        
-      def integer_validator(self, name, value):
-            """This is an integer validator that assign value"""
-            if not isinstance(value, int):
-             raise TypeError("{} must be an integer".format(name))
-            if value <= 0:
-             raise ValueError("{} must be greater than 0".format(name)) 
-            
-      def __dir__(cls) -> None:
-                """Removing __init_subclass"""
-                attributes = super().__dir__()
-                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
-        
 class Rectangle(BaseGeometry):
         """A rectangle with width and height"""
 
@@ -112,7 +86,26 @@ class Rectangle(BaseGeometry):
                 attributes = super().__dir__()
                 return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
         
-class BaseGeometry(Rectangle):
+class BaseGeometry(metaclass=AMetaClass):
+    """Public instance method that raise an Exception"""
+
+    def area(self):
+        """Raise an exception that area is not implemented"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+            """This is an integer validator that assign value"""
+            if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name)) 
+    
+    def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+      
+class Rectangle(BaseGeometry):
       """A subclass"""
       def __init__(self, width, height):
                 """The width and height integers of the rectangle"""
@@ -122,6 +115,13 @@ class BaseGeometry(Rectangle):
                 self.integer_validator("height", height)
                 self.__width = width
                 self.__height = height
+
+      def integer_validator(self, name, value):
+            """This is an integer validator that assign value"""
+            if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name))
 
 
             
