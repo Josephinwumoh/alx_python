99b835c07dd3870f65e25418e240e2463920015c
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-04 09:44:39 +0100

    A Rectangle That inherit from parent.

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -17,7 +17,33 @@ class BaseGeometry(metaclass=AMetaClass):
                 attributes = super().__dir__()
                 return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
         
-class Rectangle:
+class BaseGeometry(BaseGeometry):
+      """A class BaseGeometry with width and height"""
+
+      def __init__(self, width, height):
+                """The width and height integers of the rectangle"""
+                self.__width = 0
+                self.__height = 0
+                self.integer_validator("width", width)
+                self.integer_validator("height", height)
+                self.__width = width
+                self.__height = height
+               
+        
+      def integer_validator(self, name, value):
+            """This is an integer validator that assign value"""
+            if not isinstance(value, int):
+             raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name)) 
+            
+      def __dir__(cls) -> None:
+                """Removing __init_subclass"""
+                attributes = super().__dir__()
+                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+        
+class Rectangle(BaseGeometry):
         """A rectangle with width and height"""
 
         def __init__(self, width, height):
