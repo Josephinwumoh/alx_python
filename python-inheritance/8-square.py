7f18a0594697fe1af1e634dd803f89b164e3c7aa
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-05 02:32:43 +0100

    S

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -8,7 +8,8 @@ class AMetaClass(type):
         def __dir__(cls) -> None:
                 """Removing __init_subclass"""
                 attributes = super().__dir__()
-                return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+                return [attribute for attribute in super().__dir__()
+                 if attribute != '__init_subclass__']
 
 
 
