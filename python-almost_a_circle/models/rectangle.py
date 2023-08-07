f58ae9a3fb0b008782d709849bc98c11a7a889c7
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-07 13:27:22 +0100

    Display #1.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -94,4 +94,4 @@ class Rectangle(Base):
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
-            print('' * self.__x + '#' * self.__width) 
+            print( ' ' *  self.__x  +  '#'  *  self.__width)
