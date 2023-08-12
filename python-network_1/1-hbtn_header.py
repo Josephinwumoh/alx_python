c714c38bc06b4144b9a8de929c96960c78b51173
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-12 06:59:17 +0100

    Header Response.

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,18 +5,25 @@ the value of the variable X-Request-Id"""
 import requests
 import sys
 
+
+
 def main():
     """The method use to get the header value"""
     if len(sys.argv) != 2:
-        print("Usage: pyton 1-hbtn_header.py https://intranet.hbtn.io")
+        print("Usage: python 1-hbtn_header.py https://intranet.hbtn.io")
         return
     
     url = sys.argv[1]
     response = requests.get(url)
-
-    if "X-Request-Id" in response.headers:
-        x_request_id = response.headers.get['X-Request-Id']
-        print(x_request_id)
+    
+    if response.status_code == 200:
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id header not found in response.")
+    else:
+        print("Request failed with status code:", response.status_code)
 
 if __name__ == "__main__":
     main()
