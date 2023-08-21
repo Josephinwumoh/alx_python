20104b9afa10abbe1c5226bacfaf8266f72ebd67
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-21 03:55:07 +0100

    Object_Relational_Mappers.

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,11 +4,3 @@ import requests
 
 req = 'https://alu-intranet.hbtn.io/status'
 response = requests.get(req)
-
-print("Body response:")
-print(f"\t- type: {type(response.text)}")
-print(f"\t- content: {response.text}")
-
-
-
-
