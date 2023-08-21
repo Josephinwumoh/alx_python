022f686a32e31d8f259c0aac2e8cdf03be78410d
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-21 11:50:39 +0100

    Lists of states.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-
 """A module that lists all of the states from the database hbtn_0e_0_usa"""
-import sys
 import MySQLdb
+import sys
 
-def list_states(username, password, database):
-    """Connect to the MySQL server"""
-    db = MySQLdb.connect(host = "localhost", port = 3306, user = "Gerald", passwd = "password", db = database)
-    cursor = db.cursor()
-    """Execute the SQL query that fetches all results from the rows"""
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    """Fetch all the rows from the query result"""
-    rows = cursor.fetchall()
-    """Print the results"""
-    for row in rows:
-        print(row)
-    """Close the cursor"""
-    cursor.close()
-    """Close the database"""
-    db.close()
-"""The Usage"""
+
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <Gerald> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-list_states(username, password, database)
+
+    try:
+        db = MySQLdb.connect(
+            host = "localhost",
+            port = 3306,
+            user = "Gerald",
+            passwd = password,
+            db = database
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+    finally:
+        if db:
+            db.close()
