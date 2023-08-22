5b6a76e4e12666d13d314c225f8882690fa642ec
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-22 07:12:29 +0100

    Lists of states.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""A module that list all of the states from the database hbtn_0e_0_usa"""
-import MySQLdb
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
 import sys
+import MySQLdb
 
-
-def states(username, password, database):
-
-
-    """Connect to the MySQL server"""
-    db=MySQLdb.connect(host="localhost",
-        port=3306, user="mouse",
-        passwd="password", db="database")
-    cursor= db.cursor()
-
-    """Execute the SQL query"""
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    """fetch all the results"""
-    states= cursor.fetchall()
-
-    """print the results"""
-    for state in states:
-        print(state)
-
-    """Close the database"""
-    cursor.close()
-    db.close()
-    return db
-
-"""Arguments Usage"""
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} mouse password database".format(sys.argv[0]))
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="mouse",
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
