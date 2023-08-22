a60fcc5aa5e9ef46a9e02051ea1c03f87b99792d
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-22 07:05:58 +0100

    Lists of tuples.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,25 +3,28 @@
 import MySQLdb
 import sys
 
-def list_states(username, password, database):
+
+def states(username, password, database):
+
 
     """Connect to the MySQL server"""
     db=MySQLdb.connect(host="localhost",
         port=3306, user="mouse",
-        passwd="password", db="hbtn_0e_0_usa")
+        passwd="password", db="database")
     cursor= db.cursor()
 
     """Execute the SQL query"""
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     """fetch all the results"""
-    rows= cursor.fetchall()
+    states= cursor.fetchall()
 
     """print the results"""
-    for row in rows:
-        print(row)
+    for state in states:
+        print(state)
 
     """Close the database"""
+    cursor.close()
     db.close()
     return db
 
@@ -30,3 +33,4 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    states(username,password,database)
