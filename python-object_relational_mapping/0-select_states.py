9f63b4c7b9e02bd88d60562c796cfb2494a447fd
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-22 06:38:19 +0100

    States.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 def list_states(username, password, database):
 
     """Connect to the MySQL server"""
-    db = MySQLdb.connect(host = "localhost", port = 3306, user = "mouse", passwd = "password", db = database)
+    db = MySQLdb.connect(host = "localhost", port = 3306, user = "mouse", passwd = "password", db = "hbtn_0e_0_usa")
     cursor = db.cursor()
 
     """Execute the SQL query"""
@@ -23,7 +23,7 @@ def list_states(username, password, database):
     db.close()
     return db
 
-"""Now Usage"""
+"""Arguments Usage"""
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
