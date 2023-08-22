77b10b78a6f7a85b4ac44f0712d4b6e1afab001c
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-22 07:25:25 +0100

    All states.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa
-"""
-
-import sys
+"""A module that list all of the states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} mouse password hbtn_0e_0_usa".format(sys.argv[0]))
-        sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+def states(mouse, password, hbtn_0e_0_usa):
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="mouse",
-        passwd=password,
-        db="hbtn_0e_0_usa"
-    )
 
-    cursor = db.cursor()
+    """Connect to the MySQL server"""
+    db=MySQLdb.connect(host="localhost",
+        port=3306, user="mouse",
+        passwd="password", db="hbtn_0e_0_usa")
+    cursor= db.cursor()
+
+    """Execute the SQL query"""
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
+
+    """fetch all the results"""
+    states= cursor.fetchall()
+
+    """print the results"""
     for state in states:
         print(state)
 
+    """Close the database"""
     cursor.close()
     db.close()
+
+"""Arguments Usage"""
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
