b87871e796ddfec74402eb2c1398d61c129f822a
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-23 04:13:00 +0100

    States

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""A module that lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 
 """Connect to the MySQL server"""
-db = MySQLdb.connect(host=localhost,
-                     port=3306, user=mouse,
-                     passwd=password, db=hbtn_0e_0_usa)
-cursor= db.cursor()
-
-"""Execute the SQL query"""
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-"""fetch all the results"""
-states= cursor.fetchall()
-
-"""print the results"""
-for state in states:
-    print(state)
-
-"""Close the database"""
-cursor.close()
-db.close()
-
 if __name__ =="__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user='mouse',
+                         passwd='password', 
+                         db='hbtn_0e_0_usa')
+    cursor= db.cursor()
 
+    """Execute the SQL query"""
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    """fetch all the results"""
+    states= cursor.fetchall()
+
+    """print the results"""
+    for state in states:
+        print(state)
+
+    """Close the database"""
+    cursor.close()
+    db.close()
