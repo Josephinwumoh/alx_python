a805f72aa612789cf6fc3ee15690041f94f7e6b9
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-23 02:59:13 +0100

    Lists of states.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,13 @@ import MySQLdb
 db = MySQLdb.connect(host="localhost",
                      user="mouse",
                      passwd="password",
-                     db="database")
+                     db="hbtn_0e_0_usa")
 
 cursor = db.cursor()
 
-cursor.execute("SELECT * FROM rows ORDER BY id ASC")
+cursor.execute("SELECT * FROM rows")
 rows= cursor.fetchall()
 
-for row in rows:
-    print(row)
+print(rows)
 cursor.close()
 db.close()
