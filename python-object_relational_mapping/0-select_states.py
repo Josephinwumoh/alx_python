e0eefb99fd855c02c8ee4ba334af0cda68973f6b
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-23 03:58:10 +0100

    States.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,39 +4,29 @@
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
-    
-    # Get arguments
+
+"""Connect to the MySQL server"""
+db = MySQLdb.connect(host="localhost",
+                     port=3306, username="mouse",
+                     passwd="password", db="hbtn_0e_0_usa")
+cursor= db.cursor()
+
+"""Execute the SQL query"""
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+"""fetch all the results"""
+states= cursor.fetchall()
+
+"""print the results"""
+for state in states:
+    print(state)
+
+"""Close the database"""
+cursor.close()
+db.close()
+
+if __name__ =="__main__":
     username = sys.argv[1]
-    password = sys.argv[2]
+    passwd = sys.argv[2]
     db = sys.argv[3]
-    
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user="mouse", passwd="password", db="hbtn_0e_0_usa")
-        
-        # Create a cursor object to interact with the database
-        cursor = db.cursor()
-        
-        # Execute the SQL query
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        
-        # Fetch all the rows
-        states = cursor.fetchall()
-        
-        # Print the results
-        for state in states:
-            print(state)
-        
-        # Close the cursor and database connection
-        cursor.close()
-        db.close()
-    
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL: {}".format(e))
-        sys.exit(1)
 
