7c147d4a4e71580daf6a61651aea349085a59904
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-23 17:58:52 +0100

    Filter_states.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,44 +6,14 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-            print(
-            "Usage: {} <mysql username> <mysql password> <database name>"
-            .format(sys.argv[0]))
-            sys.exit(1)
+    """Getting credentials from mysql command arguments"""
 
-    # Get arguments
-    mouse = sys.argv[1]
-    password = sys.argv[2]
-    hbtn_0e_0_usa = sys.argv[3]
+    db = MySQLdb.connect(user= sys.argv[1],
+                         password=sys.argv[2],
+                         db= sys.argv[3])
 
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host='localhost',
-                             port=3306, user=mouse,
-                             passwd=password,
-                             db=hbtn_0e_0_usa)
-
-        # Create a cursor object to interact with the database
-        cursor = db.cursor()
-
-        # Execute the SQL query
-        cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'n%'ORDER BY id ASC"
-)
-
-        # Fetch all the rows
-        states = cursor.fetchall()
-
-        # Print the results
-        for state in states:
-            print(state)
-
-        # Close the cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL: {}".format(e))
-        sys.exit(1)
+    cursor = db.cursor()
+    """Retriving all states queries by id"""
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    (print (state) for state in cursor.fetchall() if state[1][0] == "N")
+     
