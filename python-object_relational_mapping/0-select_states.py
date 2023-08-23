2551b411bd34a5b3311ebecfce48a4c7b53826cc
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-23 02:04:39 +0100

    Lists of states.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A module that list all of the states from the database"""
+"""A module that list all of the states from the hbtn_0e_0_usa"""
 import MySQLdb
 
 """Connect to the MySQL server"""
 db = MySQLdb.connect(host="localhost",
                     port=3306, user="mouse",
-                    passwd="password", db="database")
+                    passwd="password", db="hbtn_0e_0_usa")
 cursor= db.cursor()
 
 cursor.execute("SELECT * FROM states ORDER BY id ASC")
