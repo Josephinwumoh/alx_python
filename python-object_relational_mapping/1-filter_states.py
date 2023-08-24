30a24a3593f0069dc82fb10cebb57bdf34d49068
Josephinwumoh <Jinwumoh@gmail.com>
2023-08-24 06:12:15 +0100

    Lists of states.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,11 +2,11 @@
 """A module that lists all states
 with a name starting with N (upper N)"""
 
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check for correct number of arguments
     if len(sys.argv) != 4:
         print(
 
@@ -18,22 +18,19 @@ if __name__ == "__main__":
 """)
         sys.exit(1)
 
-    """Get arguments"""
-
     mouse = sys.argv[1]
     password = sys.argv[2]
     hbtn_0e_0_usa = sys.argv[3]
 
     try:
-
-        # Connect to MySQL server
+        # Connect to the MySQL server
         db = MySQLdb.connect(host="localhost",
                              port=3306, user=mouse,
                              passwd=password,
                              db=hbtn_0e_0_usa)
         cursor = db.cursor()
 
-        # Query to select states starting with 'N' (case Sensitive)
+        # Execute the query to retrieve states starting with 'N' and order by id
         query = """
 
         SELECT *
@@ -41,8 +38,6 @@ if __name__ == "__main__":
         WHERE name LIKE 'N%'
         ORDER BY id ASC;
         """
-
-        # Execute the query
         cursor.execute(query)
 
         # Fetch and display the results
@@ -50,7 +45,7 @@ if __name__ == "__main__":
         for row in results:
             print(row)
 
-        # Close the cursor and connection
+        # Close the cursor and the database connection
         cursor.close()
         db.close()
 
