d635a5660204db87f263d85930134b8d65a11894
Josephinwumoh <Jinwumoh@gmail.com>
2023-10-07 07:23:50 +0100

    Export_to_CSV.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,56 +1,20 @@
-import requests
+#!/usr/bin/python3
+# csv exported
 import csv
-import sys
-import os
+from requests import get
+from sys import argv
 
 
-def my_todo_list(employee_id):
-    """Defining the API endpoint for employee"""
-    employee_url =  f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-
-    try:
-        """ Getting the employee details"""
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-        employee_name = employee_data.get("name")
-
-        """Getting Todo list"""
-        todo_response = requests.get(todo_url)
-        todo_list = todo_response.json()
-
-        """Creating a CSV file with the user ID as variable """
-        filename = f"{employee_id}.csv"
-        with open(filename, mode='w', newline='') as scv_file:
-            fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-            writer.writeheader()
-            for task in todo_list:
-                writer.writerow({
-                    "USER_ID": employee_id,
-                    "USERNAME": employee_name,
-                    "TASK_COMPLETED_STATUS": str(task["completed"]),
-                    "TASK_TITLE": task["title"]
-                })
-        
-        print(f"Data exported to {filename}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    """Checking if CSV file exists"""
-    filename = f"{employee_id}.csv"
-    if os.path.exists(filename):
-        my_todo_list(employee_id)
-    else:
-        print(f"CSV file for employee {employee_id} does not exist.")
+def csvWrite(user):
+    """ Writing to csv """
+    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user)).json()
+    name = get('https://jsonplaceholder.typicode.com/user/{}'.format(
+        user)).json().get('username')
+    employee_data = open('{}.csv'.format(user), 'w')
+    seawrite = csv.writer(employee_data, quoting=csv.QUOTE_ALL)
+    for line in data:
+        linemate = [line.get('used_Id'), name,
+                    line.get('completed'), line.get('title')]
+        seawrite.writerow(linemate)
+    employee_data.close()
---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,81 @@
+"""Get the employee data and export them to
+CSV and JSON format
+"""
+
+import requests
+import csv
+import sys
+import json
+
+
+def my_todo_list(employee_id):
+    """Defining the API endpoint for employee"""
+    employee_url =  f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    try:
+        """ Getting the employee details"""
+        response_employee = requests.get(employee_url)
+        response_employee.raise_for_status()
+        employee_data = response_employee.json()
+
+        """ Get emloyee list"""
+        response_todo = requests.get(todo_url)
+        response_todo.raise_for_status()
+        todo_data = response_todo.json()
+
+        return employee_data, todo_data
+    
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+def export_to_csv(employee_id, employee_name, todo_data):
+    csv_filename = f"{employee_id}.csv"
+
+    with open(csv_filename, mode="w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        """ Writing the CSV header """
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        """ Writing the tasks data """
+        for todo in todo_data:
+            csv_writer.writerow([employee_id, employee_name, todo["completed"], todo["title"]])
+
+def export_to_json(employee_id, employee_name, todo_data):
+    json_filename = f"{employee_id}.json"
+    json_data = {
+        "USER_ID": [
+            {
+                "task": todo["title"],
+                "completed": todo["completed"],
+                "username": employee_name
+            }
+            for todo in todo_data
+        ]
+    }
+    with open(json_filename, "w") as json_file:
+        json.dump(json_data, json_file)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+        employee_id = int(sys.argv[1])
+        employee_data, todo_data = get_employee_data(employee_id)
+
+        """Extracting employee info"""
+        employee_name = employee_data.get("name")
+
+        """Exporting data to CSV"""
+        export_to_csv(employee_id, employee_name, todo_data)
+        print(f"Data export to {employee_id}.csv")
+
+        """Exporting data to JSON"""
+        export_to_json(employee_id, employee_name, todo_data)
+        print(f"Data exported to {employee_id}.json")
+
+if __name__ == "__main__":
+    main()
