99d824e3e6e52c77aa0b9c4b4b206c93e8a3e823
Josephinwumoh <Jinwumoh@gmail.com>
2023-10-07 07:49:41 +0100

    Gather_data_from_an_API.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,36 @@
-"""Gather data from an API"""
-
 import requests
 import sys
 
-def find_data(id):
-    data_url =  f"https://jsonplaceholder.typicode.com/users/{id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-    result = 0
 
-    """ Get the data """
-    data = requests.get(data_url)
-    todo = requests.get(todo_url)
 
-    """ Parse the data """
-    json_result = data.json()
-    employee_name = json_result["name"]
-    json_todo = todo.json()
-    complete_tasks = len(json_todo)
+def  get_data(id):
+    user_data_url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    user_todos_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    output = 0
 
-    """ Show the completed tasks"""
-    J = []
-    for task in todo.json():
+    # Fetch  data
+    user_data = requests.get(user_data_url)
+    user_todo = requests.get(user_todos_url)
+
+    # Parse data
+    json_output = user_data.json()
+    emp_name = json_output["name"]
+    json_user_todo = user_todo.json()
+    total_tasks = len(json_user_todo)
+
+    # Display the titles of completed tasks
+    m = []
+    for task in user_todo.json():
         if task["completed"]:
-            result += 1
-            J.append(task["title"])
+            output += 1
+            m.append(task["title"])
 
-    print(f"Employee {employee_name} is done with tasks ({result}/{complete_tasks}):")
-    for task_title in J:
-        print(f"\t{task_title}")
 
-if __name__ == "__main__":
+    print(f'Employee {emp_name} is done with tasks ({output}/{total_tasks}):')
+    for task_title in m:
+        print(f'\t{task_title}')
+
+
+if __name__ =='__main__':
     id = sys.argv[1]
-    find_data(id)
+    get_data(id)
