9fa2e5bdbc93fd7adc91ff2d0975568d24ecac8f
Josephinwumoh <Jinwumoh@gmail.com>
2023-10-08 04:28:27 +0100

    Gather_data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,26 @@
 import requests
 import sys
 
-def gather_todo_progress(employee_id):
+def Etodo(id):
     """Define the API endpoints for employee details and TODO list"""
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    id_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
     try:
-        # Fetch employee details
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-        employee_name = employee_data["name"]
+        """ Get the employee data"""
+        response_id = requests.get(id_url)
+        id_data = response_id.json()
+        employee_name = id_data["name"]
 
-        # Fetch TODO list
+        """ Todo Lists """
         todo_response = requests.get(todo_url)
         todo_list = todo_response.json()
 
-        # Calculate progress
+        """ What should be completed """
         total_tasks = len(todo_list)
         completed_tasks = sum(1 for task in todo_list if task["completed"])
 
-        # Display progress in the required format
+        """ Show the completed tasks"""
         print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
         for task in todo_list:
             if task["completed"]:
@@ -35,5 +35,5 @@ if __name__ == "__main__":
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    gather_todo_progress(employee_id)
+    id = int(sys.argv[1])
+    Etodo(id)
