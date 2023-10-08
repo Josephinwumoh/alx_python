991a58295ae1b5afe00cb8f42ff199fbe4363637
Josephinwumoh <Jinwumoh@gmail.com>
2023-10-08 05:42:29 +0100

    Dict_list

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,39 @@
-import json, requests, sys
+#!/usr/bin/python3
+"""
+Export data in the JSON format.
+"""
 
-def get_employee_data(employee_id):
-    # URL to fetch employee details for all employees
-    employee_url = f"https://jsonplaceholder.typicode.com/users"
-    
-    try:
-        # Fetch data for all employees
-        response_employee = requests.get(employee_url)
-        response_employee.raise_for_status()
-        employee_data = response_employee.json()
-        
-        all_data = {}
-        
-        # Iterate over all employees
-        for employee in employee_data:
-            employee_id = employee["id"]
-            employee_name = employee["name"]
-            
-            # Fetch to do list
-            _, todos_data = fetch_todos(employee_id)
-            
-            # Extract completed tasks for the employee
-            completed_tasks = [{"username": employee_name, "task": todo["title"], "completed": todo["completed"]} for todo in todos_data if todo["completed"]]
-            
-            all_data[employee_id] = completed_tasks
-            
-        return all_data
-    except requests.exceptions.HTTPError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-        
-def fetch_todos(employee_id):
-    # URL to fetch employee's to do list
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    
-    try:
-        response_todos = requests.get(todos_url)
-        response_todos.raise_for_status()
-        todos_data = response_todos.json()
-        
-        return todos_data
-    except requests.exceptions.HTTPError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-        
-def main():
-    all_data = get_employee_data()
-    
-    # Export data to json
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_data, json_file)
+import json
+import requests
 
 if __name__ == "__main__":
-    main()
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    users_response = requests.get(users_url)
+    todos_response = requests.get(todos_url)
+
+    users_data = users_response.json()
+    todos_data = todos_response.json()
+
+    all_employee_tasks = {}
+
+    for user in users_data:
+        user_id = user.get('id')
+        username = user.get('username')
+
+        user_tasks = []
+
+        for task in todos_data:
+            if task.get('userId') == user_id:
+                user_task = {
+                    'username': username,
+                    'task': task.get('title'),
+                    'completed': task.get('completed')
+                }
+                user_tasks.append(user_task)
+
+        all_employee_tasks[user_id] = user_tasks
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(all_employee_tasks, json_file)
